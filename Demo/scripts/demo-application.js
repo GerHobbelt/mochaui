8122de531b523cbb9ce444a65cbebb7b6668ea1e
Ger Hobbelt <ger@hobbelt.com>
2011-12-18 07:23:20 +0100

    corrected menu entries to properly state what the linked test is about.

--- Demo/scripts/demo-application.js
+++ Demo/scripts/demo-application.js
@@ -57,7 +57,7 @@ Demo.initializeDesktop = function(){
 							{text:'Starters',items:[
 								{text:'Virtual Desktop',url:'{demo}demo-virtual-desktop.html',target:"_blank"},
 								{text:'Fixed Width',url:'{demo}demo-fixed-width.html',target:"_blank"},
-								{text:'Fixed Width 2',url:'{demo}demo-taskbar-only.html',target:"_blank"},
+								{text:'Only Taskbar',url:'{demo}demo-taskbar-only.html',target:"_blank"},
 								{text:'Fixed No Desktop',url:'{demo}demo-fixed-no-desktop.html',target:"_blank"},
 								{text:'No Desktop',url:'{demo}demo-no-desktop.html',target:"_blank"},
 								{text:'Modal Only',url:'{demo}demo-modal-only.html',target:"_blank"}
